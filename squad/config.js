0b4da0fd746a11e80e8eeb1a989fd4cb789fa62a
Abrham Negash Gelan <abrhamnegashgelan@gmail.com>
2026-06-18 02:01:02 -0400

    Connect Squad prediction API

--- squad/config.js
+++ squad/config.js
@@ -6,6 +6,7 @@ window.WC_ANG_CONFIG = {
   tournamentName: "World Cup 2026 Prediction League",
   leaderboardCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSE82IOywT0dgnYZtt1gf4DVNDejkFf0HyXT7rtOWr1j5G288jfSC0tTy3OKvmUtTrmjPFKR-j5ZZw4/pub?gid=1685826949&single=true&output=csv",
   predictionPulseCsvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSE82IOywT0dgnYZtt1gf4DVNDejkFf0HyXT7rtOWr1j5G288jfSC0tTy3OKvmUtTrmjPFKR-j5ZZw4/pub?gid=1661493303&single=true&output=csv",
+  predictionSubmitUrl: "https://script.google.com/macros/s/AKfycbxdI4c_Rjezst04ZZmamnmQv5TXnwffaOfXUbUyQd4ZoxPrnuqAVzj0Zrwwl5fG4fQC/exec",
   enablePredictionSubmissions: true,
   resultsCsvUrl: "",
   resultsLimit: 9,
